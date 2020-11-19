ca5975fd797723666afbd2e29a6eabd04fb0983a
Zhu Weiji <zhuweiji1997@gmail.com>
2020-11-19 23:00:29 +0800

    FileIO on Students

--- src/com/user/Main.java
+++ src/com/user/Main.java
@@ -6,7 +6,10 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Main {
-    public static Path datadir = Paths.get(System.getProperty("user.dir"), "data");
+    public static final Path datadir = Paths.get(System.getProperty("user.dir"), "data");
+    public static final Path usercredpath = Paths.get(datadir.toString(), "user_cred.txt");
+    public static final Path userinfopath = Paths.get(datadir.toString(), "user_info.txt");
+
     public static void main(String[] args) {
         CommandInterface commandInterface = CommandInterface.startInterface();
 
--- src/com/user/UserFactory.java
+++ src/com/user/UserFactory.java
@@ -4,21 +4,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class UserFactory {
-    private final Path datadir = Main.datadir;
-    private final Path userinfopath = Paths.get(datadir.toString(), "user_cred");
-    public User getUser(String username, String userPermissions){
+    private static final Path datadir = Main.datadir;
+    private static final Path userinfopath = Paths.get(datadir.toString(), "user_cred");
+    public static User getUser(String username, String userPermissions){
         String[] user_info = readDB(username, userinfopath);
         if (userPermissions.equalsIgnoreCase("admin")){
-//            return new Admin(user_info);
+//            return new Admin(user_info.txt);
             return null;
         }
         else if (userPermissions.equalsIgnoreCase("student")){
-//            return new Student(user_info);
+//            return new Student(user_info.txt);
             return null;
         }
         return null;
     }
-    private String[] readDB(String username, Path filepath){
+    private static String[] readDB(String username, Path filepath){
         //todo fill in
         return null;
     }
