82c012b505fe046bf2869e39b73a5d954d551541
Zhu Weiji <zhuweiji1997@gmail.com>
2020-11-22 04:41:11 +0800

    //

--- src/com/user/Main.java
+++ src/com/user/Main.java
@@ -8,10 +8,46 @@ import java.util.Arrays;
 public class Main {
     public static final Path datadir = Paths.get(System.getProperty("user.dir"), "data");
     public static final Path usercredpath = Paths.get(datadir.toString(), "user_cred.txt");
-    public static final Path userinfopath = Paths.get(datadir.toString(), "user_info.txt");
-
+    public static final Path studentinfopath = Paths.get(datadir.toString(), "student_info.txt");
+    public static final Path admininfopath = Paths.get(datadir.toString(), "admin_info.txt");
     public static void main(String[] args) {
-        CommandInterface commandInterface = CommandInterface.startInterface();
+        LogInHandler loginhandler = LogInHandler.startHandler();
+        CommandInterface cmd = CommandInterface.getInstance();
+
+        String username = cmd.input("Enter your username: ");
+        String password;
+        if (cmd.consoleAvail()){
+            char[] passwordArr = cmd.secretInput("Enter your password: ");
+            password = new String(passwordArr);
+        }
+        else{
+            password = cmd.input("Enter your password: ");
+        }
+        boolean logged_in = loginhandler.login(username,password);
+//        String username;
+//        String password;
+        int attempts = 5;
+        while (!logged_in){
+            if (attempts <= 0){
+                cmd.display("You have entered too many wrong attempts. The program will now exit");
+                System.exit(-1);
+                break;
+            }
+            cmd.display("You have entered the wrong username or password");
+            attempts--;
+            cmd.display("You have "+ attempts+" attempts left.");
+
+            username = cmd.input("Enter your username: ");
+            if (cmd.consoleAvail()){
+                char[] passwordArr = cmd.secretInput("Enter your password: ");
+                password = new String(passwordArr);
+            }
+            else{
+                password = cmd.input("Enter your password: ");
+            }
+            logged_in = loginhandler.login(username, password);
+        }
+
 
     }
 }
--- src/com/user/UserFactory_deprec.java
+++ src/com/user/UserFactory_deprec.java
@@ -3,17 +3,17 @@ package com.user;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class UserFactory {
+public class UserFactory_deprec {
     private static final Path datadir = Main.datadir;
     private static final Path userinfopath = Paths.get(datadir.toString(), "user_cred");
-    public static User getUser(String username, String userPermissions){
+    public static UserAcc getUser(String username, String userPermissions){
         String[] user_info = readDB(username, userinfopath);
         if (userPermissions.equalsIgnoreCase("admin")){
-//            return new Admin(user_info.txt);
+//            return new Admin(student_info.txt);
             return null;
         }
         else if (userPermissions.equalsIgnoreCase("student")){
-//            return new Student(user_info.txt);
+//            return new Student(student_info.txt);
             return null;
         }
         return null;
