d495796192a8ba8f8f1773973d3e113b5950a21c
Zhu Weiji <zhuweiji1997@gmail.com>
2020-11-23 00:25:21 +0800

    //

--- src/com/course/LessonData.java
+++ src/com/course/LessonData.java
@@ -1,11 +1,15 @@
 package com.course;
-
-
-import java.io.*;
-import java.text.*;
-import java.util.*;
-
 import uml.Lesson;
+
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class LessonData {
 	
---
+++ src/com/lesson/Lesson.java
@@ -0,0 +1,14 @@
+package com.lesson;
+
+import java.util.*;
+
+public interface Lesson {
+	public void setDay();
+	public String getDay();
+	public void setStartTime(); 
+	public Calendar getStartTime();
+	public void setEndTime();
+	public Calendar getEndTime();
+	public void setVenue();
+	public String getVenue();
+}
