88089fff80b7f9dc26c4cef575e86d4498545138
weehsuan <weehsuan45@gmail.com>
2020-11-25 17:24:27 +0800

    Latest

---
+++ src/com/Main.java
@@ -0,0 +1,51 @@
+package com;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.*;
+import java.io.*;
+
+import com.course.*;
+
+public class Main {
+    public static Path cwd;
+    public static Path datadir = null;
+    public static Path usercredpath;
+    public static Path studentinfopath;
+    public static Path admininfopath;
+    public static Path courseinfopath;
+    public static Path indexinfopath;
+    public static Path studentcoursepath;
+
+    public static void main(String[] args)throws ParseException, IOException {
+
+        get_data_paths();
+
+        StudentData.initStudents();
+		CourseData.initCourses();
+		IndexData.initIndex();
+		LessonData.initLessons();
+		StudentCourseData.initStudentCourses();
+        AccountData.initAccounts();
+
+
+        LogInHandler loginhandler = LogInHandler.startHandler();
+
+        loginhandler.run();
+    }
+    private static void get_data_paths(){
+        cwd = Paths.get(System.getProperty("user.dir"));
+        if (ConsoleUserInterface.getInstance().consoleAvail()){
+            cwd = cwd.getParent();
+        }
+        datadir = Paths.get(cwd.toString(), "data");
+        usercredpath = Paths.get(datadir.toString(), "user_cred.txt");
+        studentinfopath = Paths.get(datadir.toString(), "student_info.txt");
+        admininfopath = Paths.get(datadir.toString(), "admin_info.txt");
+        courseinfopath = Paths.get(datadir.toString(), "Course.txt");
+        indexinfopath = Paths.get(datadir.toString(), "Index.txt");
+        studentcoursepath = Paths.get(datadir.toString(),"StudentCourse.txt");
+    }
+}
+
+
